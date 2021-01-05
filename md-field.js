ec8ef4f411a51e51874d731766ff8f10e38b6282
Joel Varty <joel@agilitycms.com>
2021-01-04 20:50:40 -0500

    1

--- md-field.js
+++ md-field.js
@@ -1,48 +1,62 @@
-var MarkdownField = function () {
-    var self = this;
+//
+// API Item Picker
+//
+var MarkdownCustomField = function () {
+	/// <summary>The type definition of this Agility Custom Field Type.</summary>
+	var self = this;
 
-    self.Label = "Markdown Field";
-    self.ReferenceName = "MarkdownField";
+	/// <field name="Label" type="String">The display name of the Custom Field Type</field>
+	self.Label = "Markdown";
 
-    self.Render = function (options) {
-        /// <summary>
-        ///  The Render handler for this field.  Create any elements and bindings that you might need, pull down resources.
-        /// </summary>
-        /// <param name="options" type="ContentManager.Global.CustomInputFieldParams">The options used to render this field.</param>
-    }
+	/// <field name="ReferenceName" type="String">The internal reference name of the Custom Field Type. Must not contain any special characters.</field>
+	self.ReferenceName = "Markdown";
 
-    /// <field name="Template" type="String">The partial HTML template that represents your custom field. Your ViewModel will be automatically bound to this template.</field>
-    self.Template = 'FriendlyURLTemplate';
-
-    /// <field name="DepenenciesJS"> type="Array">The Javscript dependencies that must be loaded before your ViewModel is bound. They will be loaded in the order you specify.</field>
-    self.DependenciesJS = [];
-
-    /// <field name="DepenenciesCSS" type="Array">The CSS dependencies that must be loaded before your ViewModel is bound. They will be loaded in the order you specify.</field>
-    self.DependenciesCSS = [];
+	/// <field name="Render" type="Function">This function runs every time the field is rendered</field>
+	self.Render = function (options) {
+		/// <summary>
+		///  The Render handler for this field.  Create any elements and bindings that you might need, pull down resources.
+		///  This method will be called everytime to the field value changes.
+		/// </summary>
+		/// <param name="options" type="ContentManager.Global.CustomInputFieldParams">The options used to render this field.</param>
 
 
-    /// <field name="ViewModel" type="KO ViewModel">The KO ViewModel that will be binded to your HTML template</field>
-    self.ViewModel = function (options) {
-        /// <summary>The KO ViewModel that will be binded to your HTML template.</summary>
-        /// <param name="options" type="Object">
-        ///     <field name="$elem" type="jQueryElem">The .field-row jQuery Dom Element.</field>
-        ///     <field name="contentItem" type="ContentItem Object">The entire Content Item object including Values and their KO Observable properties of all other fields on the form.</field>
-        ///     <field name="fieldBinding" type="KO Observable">The value binding of thie Custom Field Type. Get and set this field's value by using this property.</field>
-        ///     <field name="fieldSetting" type="Object">Object representing the field's settings such as 'Hidden', 'Label', and 'Description'</field>
-        ///     <field name="readonly" type="boolean">Represents if this field should be readonly or not.</field>
-        /// </param>
 
-        var self = this;
-        
-        self.value = options.fieldBinding;
-        self.contentID = options.contentItem.ContentID;
-        self.attrBinding = {};
+		//get our base element
+		var $pnl = $(".markdown-field", options.$elem);
 
-        if (options.fieldSetting.Settings.Required === "True") {
-            self.attrBinding['data-parsley-required'] = true;
-        }
-  }
-    
+		if ($pnl.size() == 0) {
+
+			var htmlContent = `
+				<div class="markdown-field">
+					<input style='width:100%' data-bind='value:value'>
+				</div>
+				`;
+			//pull down the html template and load it into the element
+			options.$elem.append(htmlContent)
+
+			$pnl = $(".markdown-field", options.$elem);
+
+			//bind our viewmodel to this
+			var viewModel = function () {
+
+				/// <summary>The KO ViewModel that will be binded to your HTML template.</summary>
+				/// <param name="options" type="Object">
+				///     <field name="$elem" type="jQueryElem">The .field-row jQuery Dom Element.</field>
+				///     <field name="contentItem" type="ContentItem Object">The entire Content Item object including Values and their KO Observable properties of all other fields on the form.</field>
+				///     <field name="fieldBinding" type="KO Observable">The value binding of thie Custom Field Type. Get and set this field's value by using this property.</field>
+				///     <field name="fieldSetting" type="Object">Object representing the field's settings such as 'Hidden', 'Label', and 'Description'</field>
+				///     <field name="readonly" type="boolean">Represents if this field should be readonly or not.</field>
+				/// </param>
+				var self = this;
+
+				self.value = options.fieldBinding; //.extend({ throttle: 500 });
+
+			}
+
+			ko.applyBindings(viewModel, $pnl.get(0));
+		}
+
+	}
 }
 
-ContentManager.Global.CustomInputFormFields.push(new MarkdownField());
+ContentManager.Global.CustomInputFormFields.push(new MarkdownCustomField());
